956228ba6ec2bcede789d38d96f778ab69210021
minhtrung3920 <trung392dg@gmail.com>
2019-10-02 16:27:20 +0700

    pending

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Odd(number);
+            Even(number);
+            Prime(number);
+            Console.WriteLine(" input 2nd number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} is square of {1}", Square(number), number);
+            Console.WriteLine("{0} is cube of {1}", Square(number), number);
+        }
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+        static void Odd(int number) 
+        {
+            IsOdd(number);
+            if (IsOdd(number)) {
+                Console.WriteLine("odd");
+            }
+            else {
+                Console.WriteLine("0");
+            }
+        }
+        public static bool IsEven(int number) 
+        {
+            return number % 2 == 0;
+        }
+        static void Even(int number)
+        {
+            IsEven(number);
+            if (IsEven(number))
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+        public static bool IsPrime(int number)
+        {
+            if (number < 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Prime(int number)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+        public static int Square(int number2)
+        {
+            return number2 * number2;
+        }
+        public static int Cube(int number2)
+        {
+            return number2 * number2 * number2;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result1 = Program.IsOdd(3);
+            Assert.AreEqual(true, result1);
+
+            bool result2 = Program.IsOdd(4);
+            Assert.AreEqual(false, result2);
+        }
+        [TestMethod]
+        public void TestIsEven()
+        {
+            bool result3 = Program.IsEven(5);
+            Assert.AreEqual(false, result3);
+
+            bool result4 = Program.IsEven(6);
+            Assert.AreEqual(true, result4);
+        }
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            bool result1 = Program.IsPrime(1);
+            Assert.AreEqual(true, result1);
+            bool result2 = Program.IsPrime(0);
+            Assert.AreEqual(false, result2);
+            bool result = Program.IsPrime(2);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestSquareCube()
+        {
+            int result1 = Program.Square(2);
+            Assert.AreEqual(4, result1);
+            int result2 = Program.Cube(3);
+            Assert.AreEqual(9, result2);
+        }
+
+    }
+}
