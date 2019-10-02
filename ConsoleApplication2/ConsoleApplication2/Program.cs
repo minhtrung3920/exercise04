8b2da7059a681dab3a0fd6805a610feed2df7709
minhtrung3920 <trung392dg@gmail.com>
2019-10-02 20:05:43 +0700

    pending

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            InputInt(array);
+        }
+        public static void InputInt(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                string str = Console.ReadLine();
+                arr[i] = int.Parse(str);
+            }
+        }
+        public static bool IsConstraint5(int[] array)
+        {
+            return true;
+
+        }
+    }
+}
