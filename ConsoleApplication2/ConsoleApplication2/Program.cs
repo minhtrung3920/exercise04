68760eb9bb36e6e4b732fa2ce9fefa80b1dcd078
minhtrung3920 <trung392dg@gmail.com>
2019-10-03 00:09:40 +0700

    pending

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -6,12 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            ////int Length = Convert.ToInt32(Console.ReadLine());
+            ///Length
             int[] array = new int[10];
+            Console.WriteLine("nhap phan tu mang: ");
             InputInt(array);
+            Console.WriteLine("check number 5: " );
+            check(array);
+            Console.WriteLine("check number 5 or 6: ");
+            check2(array);
+            Console.WriteLine("check number 5 and 6: ");
+            check3(array);
+            Console.WriteLine("have {0} number 5. ", count(array));
+            Console.WriteLine("have {0} number 5&6.", count2(array));
+            Console.WriteLine("{0} is sum of all number.", sum(array));
         }
         public static void InputInt(int[] arr)
         {
@@ -24,8 +36,106 @@ namespace ConsoleApplication2
         }
         public static bool IsConstraint5(int[] array)
         {
-            return true;
-
+            int num = 5;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (num == array[i])
+                {
+                    return true;
+                }
+            }
+                return false;
+        }
+        public static void check(int[] array)
+        {
+            IsConstraint5(array);
+            if (IsConstraint5(array) == true)
+            {
+                Console.WriteLine("number exist");
+            }
+            else
+            {
+                Console.WriteLine("number doesnt exist");
+            }
+        }
+        public static bool IsConstraint5or6(int[] array)
+        {
+            int num = 5; int num2 = 6;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num || array[i] == num2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void check2(int[] array)
+        {
+            if (IsConstraint5or6(array) == true)
+            {
+                Console.WriteLine("have number 5 or 6");
+            }
+            else
+            {
+                Console.WriteLine("dont have number 5 or 6");
+            }
+        }
+        public static bool IsConstraint5and6(int[] array)
+        {
+            int num = 5; int num2 = 6;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num && array[i] == num2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void check3(int[] array)
+        {
+            if (IsConstraint5and6(array) == true)
+            {
+                Console.WriteLine("have 5 and 6");
+            }
+            else
+            {
+                Console.WriteLine("dont have 5 and 6");
+            }
+        }
+        public static int count(int[] array)
+        {
+            int num = 5; int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num)
+                {
+                    count++;
+                }
+            }
+            return count++;
+        }
+        public static int count2(int[] array)
+        {
+            int num = 5; int num2 = 6; int count2 = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num || array[i] == num2)
+                {
+                    count2++;
+                }
+            }
+            return count2++;
+        }
+        public static int sum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
         }
     }
 }
