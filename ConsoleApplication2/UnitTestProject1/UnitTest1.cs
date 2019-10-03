4d644207c34861c68d7b00946c66bde8cfcdf604
minhtrung3920 <trung392dg@gmail.com>
2019-10-03 10:56:56 +0700

    done

--- ConsoleApplication2/UnitTestProject1/UnitTest1.cs
+++ ConsoleApplication2/UnitTestProject1/UnitTest1.cs
@@ -8,8 +8,55 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIsconstraint5()
         {
+            int[] arr = new int[] { 1, 2, 2, 3, 5 };
+            bool result = Program.IsConstraint5(arr);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestIsconstraint5or6()
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            bool result = Program.IsConstraint5or6(arr1);
+            Assert.AreEqual(true, result);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5};
+            bool result2 = Program.IsConstraint5or6(arr2);
+            Assert.AreEqual(true, result2);
+
+            int[] arr3 = new int[] { 1, 2, 3, 4, 7, 8, 9 };
+            bool result3 = Program.IsConstraint5or6(arr3);
+            Assert.AreEqual(false, result3);
+        }
+        [TestMethod]
+        public void TestIsconstraint5and6()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+            bool result = Program.IsConstraint5and6(arr);
+            Assert.AreEqual(true, result);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+            bool result2 = Program.IsConstraint5and6(arr2);
+            Assert.AreEqual(false, result2);
+
+            int[] arr3 = new int[] {1,23,7,8,9,10};
+            bool result3 = Program.IsConstraint5and6(arr2);
+            Assert.AreEqual(false, result3);    
+        }
+        [TestMethod]
+        public void TestCount5()
+        {
+            int[] arr = new int[] { 1, 23, 4, 5, 6, 7, 8 };
+            int result = Program.count(arr);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void testsum()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+            int result = Program.sum(arr);
+            Assert.AreEqual(21, result);
         }
     }
 }
